be8dcc4243dc1b919a4f35e8c16a462dbca974d8
Yan Lagarejo <clagarejo89@gmail.com>
2024-03-19 18:57:48 -0500

    Material Theme

--- src/auth/layout/AuthLayout.jsx
+++ src/auth/layout/AuthLayout.jsx
@@ -26,7 +26,7 @@ export const AuthLayout = ({ children, title = '' }) => {
           <Typography variant='h5' sx={{ mb: 1 }}>{ title }</Typography>
 
             
-            { children }
+          { children }
 
         </Grid>
 
---
+++ src/journal/views/NoteView.jsx
@@ -0,0 +1,46 @@
+import { SaveOutlined } from "@mui/icons-material"
+import { Button, Grid, TextField, Typography } from "@mui/material"
+import { ImageGalery } from "../components/ImageGalery"
+
+export const NoteView = () => {
+    return (
+        <Grid container direction="row" justifyContent="space-between" alignItems="center" sx={{ mb: 1 }}>
+            <Grid item>
+                <Typography fontSize={39} fontWeight="light">
+                    19 de marzo 2024
+                </Typography>
+            </Grid>
+
+            <Grid item>
+                <Button color="primary" sx={{ padding: 2 }}>
+                    <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
+                    Guardar
+                </Button>
+            </Grid>
+
+            <Grid container>
+                <TextField
+                    type="text"
+                    variant="filled"
+                    fullWidth
+                    label="Titulo"
+                    placeholder="Ingrese un titulo"
+                    sx={{ border: 'none', mb: 1 }}
+                />
+
+                <TextField
+                    type="text"
+                    variant="filled"
+                    fullWidth
+                    multiline
+                    placeholder="¿Qué sucedio hoy?"
+                    sx={{ border: 'none', mb: 1 }}
+                    minRows={ 5 }
+                />
+            </Grid>
+
+            <ImageGalery />
+
+        </Grid>
+    )
+}
